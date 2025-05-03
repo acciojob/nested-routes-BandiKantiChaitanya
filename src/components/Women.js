209cddeb13f3dd79c473cd069dd254bdc8f9f04a
Bandi Kanti Chaitanya <bandikantichaitanya2@gmail.com>
2025-05-03 17:25:24 +0530

    updated version1

--- src/components/Women.js
+++ src/components/Women.js
@@ -6,25 +6,27 @@ import Shirt from './Shirt';
 import Jewellery from './Jewellery';
 
 function Women() {
-  const { path, url } = useRouteMatch(); // 👈 dynamic matching
+  const { path, url } = useRouteMatch(); // Get the dynamic path and url
 
   return (
     <div>
       Women Items:
       <ul>
-        <li><Link to={`${url}/grooming`}>Grooming</Link></li>
-        <li><Link to={`${url}/shirt`}>Shirt</Link></li>
-        <li><Link to={`${url}/trouser`}>Trouser</Link></li>
-        <li><Link to={`${url}/jewellery`}>Jewellery</Link></li>
+        <li><Link to={`${url}/Grooming`}><a href="">Grooming</a></Link></li>
+        <li><Link to={`${url}/Shirt`}><a href="">Shirt</a></Link></li> {/* Capital S */}
+        <li><Link to={`${url}/Trouser`}><a href="">Trouser</a></Link></li> {/* Capital T */}
+        <li><Link to={`${url}/Jewellery`}><a href="">Jewellery</a></Link></li> {/* Capital J */}
       </ul>
 
+      {/* Render appropriate components based on the current path */}
       <Switch>
         <Route exact path={path}>
+          
         </Route>
         <Route path={`${path}/grooming`} component={Grooming} />
-        <Route path={`${path}/shirt`} component={Shirt} />
-        <Route path={`${path}/trouser`} component={Trouser} />
-        <Route path={`${path}/jewellery`} component={Jewellery} />
+        <Route path={`${path}/Shirt`} component={Shirt} /> {/* Capital S */}
+        <Route path={`${path}/Trouser`} component={Trouser} /> {/* Capital T */}
+        <Route path={`${path}/Jewellery`} component={Jewellery} /> {/* Capital J */}
       </Switch>
     </div>
   );
